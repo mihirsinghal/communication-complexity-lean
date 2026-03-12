07c6e98ad80c9599db4ccfdd9ecf6d0b260cdb28
Mihir Singhal <mihir.a.singhal@gmail.com>
2026-03-12 00:57:42 -0700

    fixed

--- CommunicationComplexity/Basic.lean
+++ CommunicationComplexity/Basic.lean
@@ -3,8 +3,14 @@ import CommunicationComplexity.Det.Basic
 import CommunicationComplexity.Det.Generalized
 import CommunicationComplexity.Rand.Basic
 
+open MeasureTheory
+
 noncomputable def deterministic_communication_complexity {X Y α} (f : X → Y → α) : ℕ :=
   sInf { n | ∃ p : DetProtocol X Y α, p.computes f ∧ p.complexity ≤ n }
 
 noncomputable def randomized_communication_complexity {X Y α} (f : X → Y → α) (ε : ℝ) : ℕ :=
-  sInf { n | ∃ p : RandProtocol _ _ X Y α, p.approx_computes f ε ∧ p.complexity ≤ n }
+  sInf { n | ∃ (Ω_X Ω_Y : Type*) (mX : MeasureSpace Ω_X) (mY : MeasureSpace Ω_Y)
+    (_ : @MeasureTheory.IsProbabilityMeasure Ω_X mX.toMeasurableSpace mX.volume)
+    (_ : @MeasureTheory.IsProbabilityMeasure Ω_Y mY.toMeasurableSpace mY.volume)
+    (p : @RandProtocol Ω_X Ω_Y mX mY _ _ X Y α),
+    p.approx_computes f ε ∧ p.complexity ≤ n }
